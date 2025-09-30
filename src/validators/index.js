f9729a57bdde8c28beb0572200109bf206c07e65
Y L Charan Teja <charantejayandra@gmail.com>
2025-10-01 00:37:20 +0530

    add the controllers and routes

--- src/validators/index.js
+++ src/validators/index.js
@@ -30,4 +30,34 @@ const userLoginValidator = ()=>{
     ]
 }
 
-export {userRegisterValidator,userLoginValidator};
+const userChangeCurrentPasswordValidator = ()=>{
+    return [
+        body('oldPassword')
+            .trim()
+            .notEmpty().withMessage('Old password is required'),
+        body('newPassword')
+            .trim()
+            .notEmpty().withMessage('New password is required')
+            .isLength({min:6,max:30}).withMessage('New password must be between 6 and 30 characters'),
+    ]
+}
+
+const userForgotPasswordValidator = ()=>{
+    return [
+        body('email')
+            .trim()
+            .notEmpty().withMessage('Email is required')
+            .isEmail().withMessage('Invalid email address'),
+    ]
+}
+
+const userResetForgotPasswordValidator = ()=>{
+    return [
+        body('newPassword')
+            .trim()
+            .notEmpty().withMessage('New password is required')
+            .isLength({min:6,max:30}).withMessage('New password must be between 6 and 30 characters'),
+    ]
+}
+
+export {userRegisterValidator,userLoginValidator,userChangeCurrentPasswordValidator,userForgotPasswordValidator,userResetForgotPasswordValidator};
